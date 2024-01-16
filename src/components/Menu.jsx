b4aaddad7a7b0ebea6249a1506789613be664b23
Felipe Moura Martins <123138234+PhoenixFoxz@users.noreply.github.com>
2024-01-16 11:47:28 -0300

    Criação do componente Menu

---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+export default function Menu(){
+    return (
+    <nav>
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">Equipe</a></li>
+          <li><a href="">Serviços</a></li>
+        </ul>
+      </nav>
+    )
+} 
---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import Menu from "@/components/Menu";
+
+export default function Home(){
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Menu />
+      <main>
+        <h2>Bem-vindo ao Next.js</h2>
+        <h3>Página inicial</h3>
+      </main>
+    </>
+  )
+}
